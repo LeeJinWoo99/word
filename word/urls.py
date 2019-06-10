add1c9acd82da9137b0cfad8a9877b0c1ad12395
LJWll445809! <jw558019@gmail.com>
2019-06-10 22:25:44 +0900

    second commit of wordcount proj

--- word/urls.py
+++ word/urls.py
@@ -19,7 +19,7 @@ import wordcount.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', wordcount.views.home, name='home'),
+    path('', wordcount.views.home, name="home"),
     path('about/', wordcount.views.about, name='about'),
     path('result/', wordcount.views.result, name='result'),
 ]
